680c918a7ea18e749bcaaa1a1c69e9296ce040bf
MARIIA SOKOLOVA <59738036+marias0@users.noreply.github.com>
2021-05-25 19:14:01 +0300

    Add files via upload

--- weatherDynamics.py
+++ weatherDynamics.py
@@ -2,14 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
-import time
-from datetime import date, timedelta, datetime
-from collections import Counter
+from datetime import date, timedelta
 
 from openpyxl import Workbook
 from openpyxl.styles import Font, Side, Border, Alignment, NamedStyle, PatternFill
-from openpyxl.chart import Reference, StockChart, LineChart
+from openpyxl.chart import Reference, StockChart
 
+
+# Chromedriver setup
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
@@ -19,6 +19,7 @@ driver = webdriver.Chrome(
 )
 
 
+# Function to parse data from the website
 def Data(website, find_tag, find_class, findAll_tag, findAll_class):
 
     driver.get(website)
@@ -31,6 +32,7 @@ def Data(website, find_tag, find_class, findAll_tag, findAll_class):
     return list(map(lambda x: x.text.strip(), info))
 
 
+# Making a list with 10 days weather forecast
 tenDaysWeather = Data(
     "https://www.gismeteo.ru/weather-sankt-peterburg-4079/10-days/",
     "div",
@@ -39,45 +41,24 @@ tenDaysWeather = Data(
     "unit unit_temperature_c",
 )
 
-weatherMax = tenDaysWeather[::2]
-weatherMin = tenDaysWeather[1::2]
+weatherMax = [int(i) for i in tenDaysWeather[::2]]
+weatherMin = [int(i) for i in tenDaysWeather[1::2]]
 
 
 TODAY = date.today()
-tenDays = []
-for i in range(10):
-    date = TODAY + timedelta(i + 1)
-    tenDays.append(date)
+tenDaysAhead = [TODAY + timedelta(i) for i in range(10)]
+weatherForecast = list(zip(tenDaysAhead, weatherMax, weatherMin))
 
 
-def table(days, max_weather, min_weather):
-    weather_table = []
-    for i in range(len(days)):
-        weather_table.append([days[i], max_weather[i], min_weather[i]])
-    return weather_table
+# Weather in past 10 days
+tenDaysBack = [TODAY - timedelta(i + 1) for i in range(10)]
 
 
-weatherForecast = table(tenDays, weatherMax, weatherMin)
-# print(weatherForecast)
-
-
-monthsAll = []
-for i in range(9):
-    date = TODAY - timedelta(i + 18)
-    current_date = str(date)
-    year, month, day = current_date.split("-")
-    monthsAll.append(int(month))
-
-months = sorted(Counter(monthsAll))
-print(months)
-
-weatherPast = []
-
-for month in months:
+def get_month_weather(month):
     path = "https://www.gismeteo.ru/diary/4079/2021/{query}/"
     website_path = path.format(query=month)
 
-    MonthDays = Data(
+    monthDays = Data(
         website_path,
         "div",
         "container",
@@ -85,7 +66,7 @@ for month in months:
         "first",
     )
 
-    MonthWeather = Data(
+    monthWeather = Data(
         website_path,
         "div",
         "container",
@@ -93,30 +74,25 @@ for month in months:
         ["first_in_group positive", "first_in_group"],
     )
 
-    MonthWeatherMax = MonthWeather[::2]
-    MonthWeatherMin = MonthWeather[1::2]
+    monthWeatherMax = monthWeather[::2]
+    monthWeatherMin = monthWeather[1::2]
 
-    MonthDate = []
-    for day in MonthDays:
-        Dt = datetime(int(2021), int(month), int(day))
-        D = datetime.date(Dt)
-        MonthDate.append(D)
+    return list(zip(monthDays, monthWeatherMax, monthWeatherMin))
 
-    i = 0
-    for i in range(len(MonthDate)):
-        weatherPast.append([MonthDate[i], MonthWeatherMax[i], MonthWeatherMin[i]])
 
-print(weatherPast)
+def get_day_weather(date):
+    year, month, day = str(date).split("-")
+    allMonth = get_month_weather(int(month))
+    for row in allMonth:
+        if row[0] == str(int(day)):
+            return [date, int(row[1]), int(row[2])]
 
-weatherInPast = []
-for k in range(9):
-    date = TODAY - timedelta(k + 18)
-    current_date = str(date)
-    for row in weatherPast:
-        if row[0] == date:
-            weatherInPast.append(row)
-print(weatherInPast)
 
+# Making a list with weather in 10 days past
+weatherPast = list(map(get_day_weather, tenDaysBack))
+
+
+# Creating an excel file
 wb = Workbook()
 destination_filename = "Weather.xlsx"
 
@@ -130,7 +106,7 @@ ws["C2"] = "Min"
 ws.merge_cells("A1:A2")
 ws.merge_cells("B1:C1")
 
-for row in reversed(weatherInPast):
+for row in reversed(weatherPast):
     ws.append(row)
 
 for row in weatherForecast:
@@ -139,13 +115,13 @@ for row in weatherForecast:
 ws.column_dimensions["A"].width = 11
 
 dateStyle = NamedStyle(name="dateStyle", number_format="dd.mm.yy")
-for row in ws["A3:A21"]:
+for row in ws["A3:A22"]:
     for cell in row:
         cell.style = dateStyle
 
 cellBorder = Side(style="thin", color="808080")
 
-for row in ws["A1:C21"]:
+for row in ws["A1:C22"]:
     for cell in row:
         cell.font = Font(name="Malgun Gothic")
         cell.alignment = Alignment(horizontal="center", vertical="center")
@@ -154,14 +130,15 @@ for row in ws["A1:C21"]:
         )
 
 today_fill = PatternFill(start_color="45F045", end_color="45F045", fill_type="solid")
-ws["A12"].fill = today_fill
-ws["B12"].fill = today_fill
-ws["C12"].fill = today_fill
+ws["A13"].fill = today_fill
+ws["B13"].fill = today_fill
+ws["C13"].fill = today_fill
+
 
 chartObject = StockChart()
 
-labels_reference = Reference(ws, min_row=3, min_col=1, max_row=21)
-values_reference = Reference(ws, min_row=3, min_col=2, max_row=21, max_col=3)
+labels_reference = Reference(ws, min_row=3, min_col=1, max_row=22)
+values_reference = Reference(ws, min_row=3, min_col=2, max_row=22, max_col=3)
 
 chartObject.add_data(values_reference)
 chartObject.set_categories(labels_reference)
